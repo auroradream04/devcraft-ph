99bab74513c25fba7010d8c6b7c01794999b975e
dove <codegeekdove@gmail.com>
2024-03-30 11:04:05 +0800

    fix: modal width

--- app/components/ConfirmBox/index.tsx
+++ app/components/ConfirmBox/index.tsx
@@ -18,16 +18,16 @@ export default function ConfirmBox() {
           {isCancel &&
             ' Would you like to proceed with cancelling this user form registreation?'}
           {isProceed &&
-            ' Would you like to proceed with creating this user form?'}
+            'Would you like to proceed with creating this user form?'}
         </p>
       </div>
-      <div className="gap-5 bg-gray-50 py-3 sm:flex">
+      <div className="flex flex-wrap gap-5 bg-gray-50 py-3">
         <Button
           type="button"
           title="go back"
           elTag="linkTag"
           path="?createuser=true"
-          btnStyles="bg-secondary block flex-1  rounded-lg px-4 py-2 font-bold capitalize"
+          btnStyles="bg-secondary block flex-1 rounded-lg px-4 py-2 font-bold capitalize"
         />
 
         {isCancel ? (
@@ -36,14 +36,14 @@ export default function ConfirmBox() {
             title="confirm cancel"
             elTag="linkTag"
             path="/home"
-            btnStyles="bg-primary block flex-1  rounded-lg px-4 py-2 font-bold capitalize"
+            btnStyles="bg-primary block flex-1 rounded-lg px-4 py-2 font-bold capitalize"
           />
         ) : (
           <Button
             type="submit"
             title="confirm creation"
-            elTag="btnTag"
-            btnStyles="bg-primary block flex-1  rounded-lg px-4 py-2 font-bold capitalize"
+            elTag="linkTag"
+            btnStyles="bg-primary flex-1 block rounded-lg px-4 py-2 font-bold capitalize"
           />
         )}
       </div>
--- app/components/Modal/index.tsx
+++ app/components/Modal/index.tsx
@@ -1,23 +1,14 @@
 'use client';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 
 export type ModalProps = {
   element: React.ReactNode;
   width?: string;
 };
 
-export default function Modal({ element, width = 'max-w-3xl' }: ModalProps) {
-  const router = useRouter();
+export default function Modal({ element, width = 'w-2/4' }: ModalProps) {
   const searchParams = useSearchParams();
   let modal = Boolean(searchParams.get('createuser'));
-  const pathname = usePathname();
-
-  // function handleClick(e: React.MouseEvent<HTMLDivElement>) {
-  //   const target = e.target as HTMLElement;
-  //   if (target.classList.contains('overlay')) {
-  //     router.push(pathname);
-  //   }
-  // }
 
   return (
     <>
@@ -34,7 +25,7 @@ export default function Modal({ element, width = 'max-w-3xl' }: ModalProps) {
           <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
             <div className="overlay flex cursor-default items-end justify-center p-4 text-center sm:min-h-full sm:items-center sm:p-0">
               <div
-                className={`overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:${width}`}
+                className={`overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all ${width}`}
               >
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   {element}
