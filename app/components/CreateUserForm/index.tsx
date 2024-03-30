df35d3c425995e412e605e10dbfd4cedd7468bb1
dove <codegeekdove@gmail.com>
2024-03-30 19:56:44 +0800

    feat: play around with server action and validate form with zod

--- app/components/CreateUserForm/index.tsx
+++ app/components/CreateUserForm/index.tsx
@@ -1,14 +1,27 @@
 'use client';
-
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+import { experimental_useFormState as useFormState } from 'react-dom';
 import { Button } from '../Button';
 import ConfirmBox from '../ConfirmBox';
 import Modal from '../Modal';
+import { createUser } from './action';
 
 export default function CreateUserForm() {
+  const router = useRouter();
+  const initialState = { errors: {} };
+
+  const [{ errors }, dispatch] = useFormState(createUser, initialState);
+
   const searchParams = useSearchParams();
   let isCancel = Boolean(searchParams.get('cancel'));
   let isProceed = Boolean(searchParams.get('proceed'));
+
+  useEffect(() => {
+    if (Object.keys(errors).length > 0) {
+      router.push('?createuser=true');
+    }
+  }, [errors]);
 
   return (
     <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
@@ -18,11 +31,11 @@ export default function CreateUserForm() {
       <hr className="my-2 h-px border-0 bg-gray-200 "></hr>
 
       <div>
-        <form>
+        <form action={dispatch}>
           <div className="mt-4 grid grid-cols-1 gap-x-6 gap-y-3 sm:grid-cols-6">
             <div className="col-span-full">
               <label
-                htmlFor="fisrtName"
+                htmlFor="firstName"
                 className="text-transform: block text-sm font-medium capitalize leading-6 text-gray-900"
               >
                 employee's first name
@@ -30,9 +43,18 @@ export default function CreateUserForm() {
               <div className="mt-2">
                 <input
                   type="text"
-                  id="fisrtName"
+                  name="firstName"
                   className="block w-full rounded-md border-0 px-2 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  aria-describedby="create-error"
                 />
+                <div id="create-error" aria-live="polite" aria-atomic="true">
+                  {errors?.firstName &&
+                    errors.firstName.map((error: string) => (
+                      <span className="text-red-500" key={error}>
+                        {error}
+                      </span>
+                    ))}
+                </div>
               </div>
             </div>
             <div className="col-span-full">
@@ -45,9 +67,18 @@ export default function CreateUserForm() {
               <div className="mt-2">
                 <input
                   type="text"
-                  id="lastName"
+                  name="lastName"
                   className="block w-full rounded-md border-0 px-2 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  aria-describedby="create-error"
                 />
+                <div id="create-error" aria-live="polite" aria-atomic="true">
+                  {errors?.lastName &&
+                    errors.lastName.map((error: string) => (
+                      <span className="text-red-500" key={error}>
+                        {error}
+                      </span>
+                    ))}
+                </div>
               </div>
             </div>
             <div className="col-span-full">
@@ -60,9 +91,18 @@ export default function CreateUserForm() {
               <div className="mt-2">
                 <input
                   type="email"
-                  id="email"
+                  name="email"
                   className="block w-full rounded-md border-0 px-2 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  aria-describedby="create-error"
                 />
+                <div id="create-error" aria-live="polite" aria-atomic="true">
+                  {errors?.email &&
+                    errors.email.map((error: string) => (
+                      <span className="text-red-500" key={error}>
+                        {error}
+                      </span>
+                    ))}
+                </div>
               </div>
             </div>
             <div className="col-span-full">
@@ -75,9 +115,18 @@ export default function CreateUserForm() {
               <div className="mt-2">
                 <input
                   type="password"
-                  id="password"
+                  name="password"
                   className="block w-full rounded-md border-0 px-2 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  aria-describedby="create-error"
                 />
+                <div id="create-error" aria-live="polite" aria-atomic="true">
+                  {errors?.password &&
+                    errors.password.map((error: string) => (
+                      <span className="text-red-500" key={error}>
+                        {error}
+                      </span>
+                    ))}
+                </div>
               </div>
             </div>
           </div>
@@ -102,6 +151,7 @@ export default function CreateUserForm() {
             {isProceed && <Modal element={<ConfirmBox />} width="max-w-lg" />}
           </div>
         </form>
+        {/* {state.data && <pre>{JSON.stringify(state, null, 2)}</pre>} */}
       </div>
     </div>
   );
